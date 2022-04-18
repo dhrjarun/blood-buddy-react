c27f55dfda880fc03a1bb0838770a4044bc1afe3
Dhiraj Arun <dhrjarun@gmail.com>
2022-04-18 20:07:10 +0530

    added images module with some images inside

---
+++ src/images/index.ts
@@ -0,0 +1,6 @@
+export const logoBlack = require("./logo-black.png");
+export const logoWhite = require("./logo-white.png");
+export const authCoverLg = require("./auth-cover-lg.png");
+export const authEmail = require("./auth-email.png");
+export const authSuccess = require("./auth-success.png");
+export const authFailed = require("./auth-failed.png");
