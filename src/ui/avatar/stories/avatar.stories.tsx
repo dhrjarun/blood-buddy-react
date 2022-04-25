fe9b94c4d76e86aa0292585ab2f8ee6b633dc2ea
Dhiraj Arun <dhrjarun@gmail.com>
2022-04-25 15:19:18 +0530

    fix(avatar): tserror 2590 inside avatar stories

--- src/ui/avatar/stories/avatar.stories.tsx
+++ src/ui/avatar/stories/avatar.stories.tsx
@@ -1,11 +1,10 @@
-import { ComponentMeta } from "@storybook/react";
 import { Avatar } from "../avatar";
 import { AvatarBadge } from "../avatar-badge";
 
 export default {
   title: "ui/data-display/avatar",
   component: Avatar,
-} as ComponentMeta<typeof Avatar>;
+};
 
 export const withBadge = () => {
   return (
