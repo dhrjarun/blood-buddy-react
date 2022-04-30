9d180a5e8f96365c1ff018796a6224b65fe85b1a
Dhiraj Arun <dhrjarun@gmail.com>
2022-04-30 10:17:31 +0530

    feat(button): add buttonGroup component, just exporting chakra ButtonGroup
    create the file button-group.tsx and from that exporting the buttonGroup chakra component

--- src/ui/buttons/index.ts
+++ src/ui/buttons/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./button";
 export { default as IconButton } from "./icon-button";
 export * from "./icon-button";
 export * from "./button";
+export * from "./button-group";
