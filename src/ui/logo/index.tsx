63a63d147a56f815afdfaa06d8297ad4f692b175
Dhiraj Arun <dhrjarun@gmail.com>
2022-04-30 10:34:17 +0530

    fix(logo): ts2590 error because of chakra as prop

--- src/ui/logo/index.tsx
+++ src/ui/logo/index.tsx
@@ -1,29 +1,36 @@
-import { forwardRef, Image, ImageProps, useColorMode } from "@chakra-ui/react";
+import React from "react";
+import { Image, ImageProps, useColorMode } from "@chakra-ui/react";
 import { logoBlack, logoWhite } from "images";
 
 export interface LogoProps extends ImageProps {}
 
-export const Logo = forwardRef<LogoProps, "img">((props, ref) => {
-  const { colorMode } = useColorMode();
+export const Logo = React.forwardRef<HTMLImageElement, LogoProps>(
+  (props, ref) => {
+    const { colorMode } = useColorMode();
 
-  return (
-    <Image
-      ref={ref}
-      {...props}
-      src={colorMode === "light" ? logoBlack : logoWhite}
-      px="3.5"
-    />
-  );
-});
+    return (
+      <Image
+        ref={ref}
+        {...props}
+        src={colorMode === "light" ? logoBlack : logoWhite}
+        px="3.5"
+      />
+    );
+  }
+);
 
 export interface LogoLightProps extends ImageProps {}
 
-export const LogoLight = forwardRef<LogoLightProps, "img">((props, ref) => {
-  return <Image ref={ref} {...props} src={logoWhite} px="3.5" />;
-});
+export const LogoLight = React.forwardRef<HTMLImageElement, LogoLightProps>(
+  (props, ref) => {
+    return <Image ref={ref} {...props} src={logoWhite} px="3.5" />;
+  }
+);
 
 export interface LogoDarkProps extends ImageProps {}
 
-export const LogoDark = forwardRef<LogoDarkProps, "img">((props, ref) => {
-  return <Image ref={ref} {...props} src={logoBlack} px="3.5" />;
-});
+export const LogoDark = React.forwardRef<HTMLImageElement, LogoDarkProps>(
+  (props, ref) => {
+    return <Image ref={ref} {...props} src={logoBlack} px="3.5" />;
+  }
+);
